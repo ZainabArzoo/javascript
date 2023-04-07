3059d14c148fb49eb8d8f3667e94d830b5afa766
ZainabArzoo <zainabarzoo@gmail.com>
2023-04-07 03:23:00 -1200

    J.S

---
+++ scripts/app.js
@@ -0,0 +1,112 @@
+// first task -- Date 6/4/2023
+
+// chapter 1 -- Alerts
+alert("Thanks for your input!");
+alert("Thanks for your input!");
+
+// chapter 2 -- Variables for Strings
+var name = "Mark";
+var name = "Mark";
+name = "Ace";
+var nationality;
+nationality = "U.S.";
+var floogle = "Mark";
+var name = "Mark";
+var floogle = "Mark";
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var nickname = "Bub";
+alert("Thanks for your input!");
+1 var thanx = "Thanks for your input!"
+2 alert(thanx);
+
+// chapter 3 -- Variables for Numbers
+var weight = 150;
+weight + 25
+1 var originalNum = 23;
+2 var newNum = originalNum + 7;
+1 var originalNum = 23;
+2 var numToBeAdded = 7;
+3 var newNum = originalNum + numToBeAdded;
+1 var originalNum = 90;
+2 originalNum = originalNum + 10;
+1 var originalNum = "23";
+2 var newNum = originalNum + 7;
+1 var originalNumber = 23;
+2 var newNumber = originalNumber + 7;
+var 1stPresident = "Washington";
+var prezWhoCame1st = "Washington";
+alert(144);
+var caseQty = 144;
+1 var caseQty = 144;
+2 alert(caseQty);
+
+// chapter 4 -- Variable Names Legal and Illegal
+userResponse
+userResponseTime
+userResponseTimeLimit
+response
+
+// chapter 5 -- Math expressions:Familiar operators
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2 + 2);
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+1 var num = 10;
+2 var popularNumber = num + 200;
+1 var num = 10;
+2 var anotherNum = 1;
+3 var popularNumber = num + anotherNum;
+var whatsLeftOver = 10 % 3;
+var whatsLeftOver = 9 % 3;
+
+// chapter 6 -- Math expressions:Unfamiliar operators
+num++;
+num = num + 1;
+num--;
+1 var num = 1;
+2 var newNum = num++;
+1 var num = 1;
+2 var newNum = ++num;
+1 var num = 1;
+2 var newNum = num--;
+1 var num = 1;
+2 var newNum = --num;
+
+// chapter 7 -- Math expressions:Eliminating ambiguity
+var totalCost = 1 + 3 * 4;
+var totalCost = 1 + (3 * 4);
+var totalCost = (1 + 3) * 4;
+var resultOfComputation = (2 * 4) * 4 + 2;
+resultOfComputation = ((2 * 4) * 4) + 2;
+resultOfComputation = (2 * 4) * (4 + 2);
+
+// chapter 8 -- Concatenating text strings
+alert("Thanks for your input!");
+1 var message = "Thanks for your input!";
+2 alert(message);
+alert("Thanks, " + userName + "!");
+1 var message = "Thanks, ";
+2 var banger = "!";
+3 alert(message + userName + banger);
+alert("Thanks, " + "Susan" + "!");
+1 var message = "Thanks, ";
+2 var userName = "Susan";
+3 var banger = "!";
+4 var customMess = message + userName + banger;
+5 alert(customMess);
+alert("2" + "2");
+alert("2 plus 2 equals " + 2 + 2);
+
+// chapter 9 -- Prompts
+var spec = prompt("Your species?", "human");
+1 var question = "Your species?";
+2 var defaultAnswer = "human";
+3 var spec = prompt(question, defaultAnswer);
+1 var numberOfCats = prompt("How many cats?");
+2 var tooManyCats = numberOfCats + 1;
